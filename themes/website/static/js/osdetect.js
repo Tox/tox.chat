7ca77483985d480a1edcf9c0914483b94df79260
Robin Linden <dev@robinlinden.eu>
2022-02-26 13:21:24 +0100

    Update aTox download links to v0.7.3

--- themes/website/static/js/osdetect.js
+++ themes/website/static/js/osdetect.js
@@ -98,7 +98,7 @@ if (window.navigator.userAgent.indexOf("Android") != -1) {
 		name: "atox",
 		icon: "download",
 		desc: true,
-		dlLink: "https://github.com/evilcorpltd/aTox/releases/download/v0.7.2/atox.apk",
+		dlLink: "https://github.com/evilcorpltd/aTox/releases/download/v0.7.3/atox.apk",
 	}];
 }
 
