5378b4a4e39639247b8c38e33b7bf6819ed37574
Maxim Biro <nurupo.contributions@gmail.com>
2024-12-08 12:17:40 -0500

    Remove unmaintained clients
    Toxygen and uTox haven't been updated in several years
    
    Co-authored-by: jfreegman <jfreegman@gmail.com>

--- themes/website/static/js/osdetect.js
+++ themes/website/static/js/osdetect.js
@@ -52,12 +52,6 @@ if (window.navigator.userAgent.indexOf("FreeBSD") != -1) {
 	OSName = "FreeBSD";
 
 	clients = [{
-		title: "Install uTox",
-		name: "utox",
-		icon: "external-link",
-		desc: true,
-		dlLink: "https://freshports.org/net-im/uTox/",
-	}, {
 		title: "Install Toxic",
 		name: "toxic",
 		icon: "external-link",
